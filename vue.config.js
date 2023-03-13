5d65314293e4c53f3e8a03ff50464a40215e4f25
Cristhian Daza <cristian.d.b@hotmail.com>
2023-03-12 19:13:28 -0500

    fix(*) Icon url is resolved

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  css: {
+    extract: {
+      filename: "css/[name].[contenthash:8].css",
+      chunkFilename: "css/[name].[contenthash:8].css",
+    },
+  },
+  publicPath: "./",
 });
