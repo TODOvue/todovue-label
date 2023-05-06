c5302eaafd3e26ea1a5e9cae5b39da7b01bbcc1d
Cristhian Daza <cristian.d.b@hotmail.com>
2023-05-06 13:21:01 -0500

    fix(main.js) Comments are deleted

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import { createApp } from "vue";
 import Demo from "./Demo.vue";
-import "vue-highlight-code/dist/style.css"; // Styles are imported to display the code
+import "vue-highlight-code/dist/style.css";
 import TvDemo from "todovue-demo";
-import "@/assets/scss/_global.scss"; // Styles are imported to display the code
+import "@/assets/scss/_global.scss";
 
 const app = createApp(Demo);
 app.component("TvDemo", TvDemo);
