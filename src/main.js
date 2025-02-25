245cf447c136ec99f84b1367898a69646d513a73
Cristhian Daza <cristian.d.b@hotmail.com>
2025-02-24 23:37:03 -0500

    chore(main): import global SCSS styles

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import './style.css'
 import TvLabel from './demo/Demo.vue'
 import "vue-highlight-code/dist/style.css";
+import '../src/assets/scss/_global.scss'
 
 createApp(TvLabel).mount('#tv-label')
