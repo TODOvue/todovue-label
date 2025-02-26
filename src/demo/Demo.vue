f56d403996f1975f4d24f712e882f18009f95f64
Cristhian Daza <cristian.d.b@hotmail.com>
2025-02-25 23:23:07 -0500

    fix(Demo): add version attribute to tv-demo component

--- src/demo/Demo.vue
+++ src/demo/Demo.vue
@@ -14,5 +14,6 @@ const TvLabel = defineAsyncComponent(() => import('../components/TvLabel.vue'))
     npmInstall="@todovue/tvlabel"
     sourceLink="https://github.com/TODOvue/todovue-label"
     urlClone="https://github.com/TODOvue/todovue-label.git"
+    verion="1.0.1"
   ></tv-demo>
 </template>
